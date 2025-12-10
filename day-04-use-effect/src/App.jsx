c724d05378c64778970911b1736c1592f3e3aebf
KentDoCode <villalunkent03@gmail.com>
2025-12-10 13:23:19 +0800

    Day 4 Predict Age API practice

--- day-04-use-effect/src/App.jsx
+++ day-04-use-effect/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect} from 'react'
+import './App.css';
+import Axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState("");
+  const [person, setPerson] = useState({});
+
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPerson(res.data)
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex justify-center items-center h-screen font-mono">
+    <div className='flex flex-col gap-3' >
+      <input type="text" className='border rounded-lg p-2' placeholder='Ex. Kent' onChange={ handleNameChange }/>
+      <button className='bg-blue-300 py-3 px-4 rounded-lg hover:bg-blue-400 cursor-pointer transition-colors uppercase' onClick={fetchData}> Predict Age </button>
+      <p className="">Name: { person.name }</p>
+      <p className="">Predicted Age: { person.age }</p>
+      <p className="">Count: { person.count }</p>
+
+      
+    </div>
+    </div>
   )
 }
 
